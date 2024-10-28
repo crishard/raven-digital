78563f8f2cac1a65470776b610351f0569a8d552
crishard <crislantorres9@gmail.com>
2024-10-28 10:06:46 -0300

    feature: chamando seção de clientes na página

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import Blog from './components/Blog/Blog'
+import { Clients } from './components/Clients/Clients'
 import { Footer } from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import { Services } from './components/Services/Services'
@@ -21,6 +22,7 @@ function App() {
       <Testimonials />
       <Team />
       <Blog />
+      <Clients />
       <Footer />
     </main>
   )
