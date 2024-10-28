f88bacbdb9cceca2730813fb6b356e634861dd3d
crishard <crislantorres9@gmail.com>
2024-10-28 09:52:36 -0300

    feature: adicionando os dados de servços

---
+++ src/components/Services/services.ts
@@ -0,0 +1,33 @@
+import { RiArtboard2Line, RiBarChart2Line, RiComputerLine, RiPaintBrushLine, RiTerminalWindowLine, RiVipDiamondLine } from "react-icons/ri";
+export const servicesData = [
+    {
+        title: "APP DEVELOPMENT",
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        icon: RiComputerLine ,
+    },
+    {
+        title: "WEBSITE DEVELOPMENT",
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        icon: RiTerminalWindowLine ,
+    },
+    {
+        title: "UI/UX DESIGNING",
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        icon: RiArtboard2Line ,
+    },
+    {
+        title: "DIGITAL MARKETING",
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        icon: RiVipDiamondLine ,
+    },
+    {
+        title: "SEO & CONTENT WRITING",
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        icon: RiBarChart2Line ,
+    },
+    {
+        title: "GRAPHIC DESIGNING",
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        icon: RiPaintBrushLine ,
+    },
+]
