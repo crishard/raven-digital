0d07a3b61c8c2477162a3c81154c30373f593272
crishard <crislantorres9@gmail.com>
2024-10-28 09:44:57 -0300

    configurando cores do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "secondary": "#11161C", 
+        "primary": "#0A0F14",
+        "paragraph": "#787878",
+        "letters": "#EFEFEF",
+        "white": "#FFFFFF",
+        "blue-background": "#003ADB"
+      }
+    },
   },
   plugins: [],
 }
