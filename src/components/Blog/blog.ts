b626827c34340a0563104b18519619233511cd5f
crishard <crislantorres9@gmail.com>
2024-10-28 10:04:25 -0300

    feature: criando dados do blog

---
+++ src/components/Blog/blog.ts
@@ -0,0 +1,30 @@
+import img3 from "../../assets/blog/3MAmj1ZKSZA.png";
+import img2 from "../../assets/blog/g-IR3UM2v7M.png";
+import img1 from "../../assets/blog/sDwhXKJijyE (1).png";
+
+export const blogData = [
+    {
+        img: img1,
+        title: "Techno",
+        data: "07/04/2021",
+        number: 1502,
+        subtitle: "Tik tok Banned in India",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id fringilla turpis tempor enim. Vel tristique suspendisse eget sodales egestas. Donec duis lacus, odio velit"
+    },
+    {
+        img: img2,
+        title: "Techno",
+        data: "07/04/2021",
+        number: 1502,
+        subtitle: "Lorem ipsum",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id fringilla turpis tempor enim. Vel tristique suspendisse eget sodales egestas. Donec duis lacus, odio velit"
+    },
+    {
+        img: img3,
+        title: "Techno",
+        data: "07/04/2021",
+        number: 1502,
+        subtitle: "Lorem ipsum",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id fringilla turpis tempor enim. Vel tristique suspendisse eget sodales egestas. Donec duis lacus, odio velit"
+    }
+]
