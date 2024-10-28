64ddb08f32f30a0d05de4e514448ce51d18df9e6
crishard <crislantorres9@gmail.com>
2024-10-28 10:01:45 -0300

    feature: chamando seção de testimoniais na págin

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Footer } from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import { Services } from './components/Services/Services'
+import Testimonials from './components/Testimonials/Testimonials'
 import { ScrollWorks } from './components/Works/ScrollWorks'
 import { Works } from './components/Works/Works'
 import './index.css'
@@ -15,7 +16,7 @@ function App() {
       <Services />
       <Works />
       <ScrollWorks />
-      
+      <Testimonials />
       <Footer />
     </main>
   )
