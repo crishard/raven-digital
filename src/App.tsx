81dc38384f7b8e9942b0c7c2ea19643848e2b805
crishard <crislantorres9@gmail.com>
2024-10-28 09:54:17 -0300

    feature: chamando seção de serviçso na página

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Footer } from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import { Services } from './components/Services/Services'
 import './index.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <main>
       <Header />
+      <Services />
       
       <Footer />
     </main>
