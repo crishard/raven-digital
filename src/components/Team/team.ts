7254beba2428a3f92d02e51bc9fa5b73f0f9afab
crishard <crislantorres9@gmail.com>
2024-10-28 10:03:12 -0300

    feature: criando dados do time

---
+++ src/components/Team/team.ts
@@ -0,0 +1,30 @@
+import team3 from "../../assets/team/dmitry-vechorko-yXhJ_eQK0mE-unsplash 1.png"
+import team2 from "../../assets/team/eyFbjKWlR2g.png"
+import team1 from "../../assets/team/jns8BPueAgU.png"
+
+export const team = [
+    {
+      name: "Jane Doe",
+      image: team1,
+      position: "Company / CEO",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id fringilla turpis tempor enim.",
+    },
+    {
+      name: "Jane Doe",
+      image: "https://img.freepik.com/fotos-gratis/designer-trabalhando-no-modelo-3d_23-2149371896.jpg?t=st=1729556144~exp=1729559744~hmac=5505e398b7a30bc6d95b1dc809d10a0acc2dde2c20d344d880419167d6bd96ab&w=826",
+      position: "Company / CEO",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id fringilla turpis tempor enim.",
+    },
+    {
+      name: "Jane Doe",
+      image: team2,
+      position: "Company / CEO",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id fringilla turpis tempor enim.",
+    },
+    {
+      name: "Jane Doe",
+      image: team3,
+      position: "Company / CEO",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id fringilla turpis tempor enim.",
+    }
+  ]
